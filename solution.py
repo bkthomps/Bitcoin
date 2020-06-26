98ba588119c099e60d8bc23f5341ed2e9fad45cb
bkthomps <bkthomps@edu.uwaterloo.ca>
2020-06-26 18:41:02 -0400

    Fix some pycharm complaints

--- solution.py
+++ solution.py
@@ -33,7 +33,8 @@ def sha3_224_hex(hexstr):
 # --------------------------------------------------------------------------------
 
 # Part 1:Copy and paste your parameters here
-# p,q,g are DSA domain parameters, sk_i (secret keys),pk_i (public keys),k_i (random numbers) are used in each signature and verification
+# p, q, g are DSA domain parameters,
+# sk_i (secret keys), pk_i (public keys), k_i (random numbers) are used in each signature and verification
 p = Constants.p
 q = Constants.q
 g = Constants.g
@@ -45,7 +46,8 @@ sk3 = Constants.sk3
 # --------------------------------------------------------------------------------
 
 # Part 2:Assign values that you compute to those parameters as part of your answers to (a) (b) and (c)
-# (a) list all prime factors of p-1, list 3 public keys pk_i's corresponding to sk_i's, those numbers should be decimal integers
+# (a) list all prime factors of p-1,
+# list 3 public keys pk_i's corresponding to sk_i's, those numbers should be decimal integers
 pfactor1 = 2
 pfactor2 = q
 pfactor3 = 599352188457547639693740171522680835865322184075286620256386716439921029334782998562008313995103840817116953210359643511447372101221464995653177706653918911634015555633001801773590292023083604552613918655194669929368962688659673544061885990566694774938089095597940505443430714573194211134223784784744939911387314440899638056016474270609853063142638329500429039904897328933858412897374253962878313102199163400319655392723027703101354927814377851954345336880097584669598515815463134218486896858352351187255794957262790967727451704317515673833695348341
@@ -65,10 +67,10 @@ l3 = 2048 - pk3.bit_length()
 # (b)(1)
 def buildm1():
     amt1 = BitArray(hex='04')
-    leadingZeroespk1 = ''.zfill(l1)
-    leadingZeroespk2 = ''.zfill(l2)
-    m1 = BitArray(bin=leadingZeroespk1) + BitArray(bin="{0:b}".format(pk1)) + BitArray(bin=leadingZeroespk2) + BitArray(
-        bin="{0:b}".format(pk2)) + amt1
+    leading_zeroes_pk1 = ''.zfill(l1)
+    leading_zeroes_pk2 = ''.zfill(l2)
+    m1 = BitArray(bin=leading_zeroes_pk1) + BitArray(bin="{0:b}".format(pk1)) + BitArray(
+        bin=leading_zeroes_pk2) + BitArray(bin="{0:b}".format(pk2)) + amt1
     print(len(m1))
     return m1.hex
 
@@ -76,7 +78,7 @@ def buildm1():
 m1 = buildm1()
 k1 = int(sha3_224_hex('01'), 16)
 r1 = pow(g, k1, p) % q
-z1 = sha3_224_hex(m1)[0:(int)(min(q.bit_length(), 224) / 4)]
+z1 = sha3_224_hex(m1)[0:int(min(q.bit_length(), 224) / 4)]
 s1 = (int(z1, 16) + sk1 * r1) * pow(k1, -1, q)
 
 # (b)(2)
@@ -102,10 +104,10 @@ def signAndVerify():
 # (b)(3)
 def buildm2():
     amt2 = BitArray(hex='03')
-    leadingZeroespk2 = ''.zfill(l2)
-    leadingZeroespk3 = ''.zfill(l3)
-    m2 = BitArray(bin=leadingZeroespk2) + BitArray(bin="{0:b}".format(pk2)) + BitArray(bin=leadingZeroespk3) + BitArray(
-        bin="{0:b}".format(pk3)) + amt2
+    leading_zeroes_pk2 = ''.zfill(l2)
+    leading_zeroes_pk3 = ''.zfill(l3)
+    m2 = BitArray(bin=leading_zeroes_pk2) + BitArray(bin="{0:b}".format(pk2)) + BitArray(
+        bin=leading_zeroes_pk3) + BitArray(bin="{0:b}".format(pk3)) + amt2
     print(len(m2))
     return m2.hex
 
@@ -113,22 +115,23 @@ def buildm2():
 m2 = buildm2()
 k2 = int(sha3_224_hex('02'), 16)
 r2 = pow(g, k2, p) % q
-z2 = sha3_224_hex(m2)[0:(int)(min(q.bit_length(), 224) / 4)]
+z2 = sha3_224_hex(m2)[0:int(min(q.bit_length(), 224) / 4)]
 s2 = (int(z2, 16) + sk2 * r2) * pow(k2, -1, q)
 
 
-# (c) PreImageOfPW1=h(amt0)||m1||nonce1, PreImageOfPW1=h(m1)||m2||nonce2, those two variables should be hex strings with on prefix of 0x
+# (c) PreImageOfPW1=h(amt0)||m1||nonce1, PreImageOfPW1=h(m1)||m2||nonce2,
+# those two variables should be hex strings with on prefix of 0x
 def findNonce():
-    leadingZeroes = ''.zfill(32)
+    leading_zeroes = ''.zfill(32)
     amt0 = BitArray(hex='05')
     h_amt0 = sha3_224_hex(amt0.hex)
     i = 0
-    while (True):
+    while True:
         nonce = BitArray(int=i, length=128)
         input = BitArray(hex=h_amt0) + BitArray(hex=m1) + nonce
         pw1 = sha3_224_hex(input.hex)
         i += 1
-        if (pw1[0:32] == leadingZeroes):
+        if pw1[0:32] == leading_zeroes:
             return nonce
 
 
@@ -144,7 +147,7 @@ PreImageOfPW2 = ""
 # DSA signature function, p, q, g, k, sk are integers, Message are hex strings of even length.
 def Sign(p, q, g, k, sk, Message):
     r = pow(g, k, p) % q
-    z = sha3_224_hex(Message)[0:(int)(min(q.bit_length(), 224) / 4)]
+    z = sha3_224_hex(Message)[0:int(min(q.bit_length(), 224) / 4)]
     s = (int(z, 16) + sk * r) * pow(k, -1, q)
     return r, s
 
@@ -152,14 +155,11 @@ def Sign(p, q, g, k, sk, Message):
 # DSA verification function,  p, q, g, k, pk are integers, Message are hex strings of even length.
 def Verify(p, q, g, pk, Message, r, s):
     w = pow(s, -1, q)
-    z = sha3_224_hex(Message)[0:(int)(min(q.bit_length(), 224) / 4)]
+    z = sha3_224_hex(Message)[0:int(min(q.bit_length(), 224) / 4)]
     u1 = int(z, 16) * w % q
     u2 = r * w % q
     v = (pow(g, u1, p) * pow(pk, u2, p) % p) % q
-    if (v == r):
-        return True
-    else:
-        return False
+    return v == r
 
 
 signAndVerify()
